22b1623c3ebbfdb456625d31de6d261ed2e17bcd
merc-cyy <mercymuiruri2027@u.northwestern.edu>
2024-12-29 02:31:26 +0300

    Just added the place photos. Looking good.

--- business-directory/components/PlaceItemCard.tsx
+++ business-directory/components/PlaceItemCard.tsx
@@ -1,9 +1,76 @@
 import React from 'react'
+import Image from 'next/image'
+
+
+const BASE_URL_PHOTO =
+  "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400";
+
 
 function PlaceItemCard({place}:any) {
   return (
-    <div>PlaceItemCard</div>
-  )
+    <div className="w-full z-10 border-[1px] rounded-xl shadow-md">
+      <Image
+        src={BASE_URL_PHOTO+ "&photo_reference=" + place?.photos[0]?.photo_reference + "&key=" + process.env.NEXT_PUBLIC_GOOGLE_PLACE_KEY }
+        alt="placeholder"
+        width={200}
+        height={80}
+        className="w-full h-[150px] object-cover rounded-t-xl"
+      />
+
+      <div className="p-2">
+        <h2 className="line-clamp-2">{place.name}</h2>
+        <div className="flex gap-2 mt-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 text-red-500"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+            ></path>
+
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5  "
+            ></path>
+          </svg>
+
+          <h2 className="text-[12px] text-gray-400 line-clamp-2 tracking-wider">
+            {place.formatted_address}
+          </h2>
+        </div>
+
+
+        <div className="flex gap-2 mt-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 text-red-500"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.11a.563.563 0 00.475.345"
+            ></path>
+
+          </svg>
+
+          <h2 className="text-[12px] text-gray-400 line-clamp-2 tracking-wider">
+            {place.rating}
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PlaceItemCard
--- business-directory/components/PlaceList.tsx
+++ business-directory/components/PlaceList.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import PlaceItemCard from './PlaceItemCard';
 
+
+
 function PlaceList({placeList}:any) {
   return (
-    <div className="px-[10px] md:px-[120px] mt-7">
+    <div className="px-[10px] md:px-[120px] mt-7 z-10">
       <h2 className="text-[20px] font-bold">Search Results</h2>
 
-      <div>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 '>
         {placeList.map((place: any, index: number) => (
-          <div key={index}>
+          <div key={index} className='z-10'>
             <PlaceItemCard place = {place}/>
           </div>
         ))}
